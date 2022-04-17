dd98e93ee2bf1e3719cb8acbb273efe09421d027
Bao_Worapoj <Boonchoopauto@gmail.com>
2022-04-17 12:15:35 +0700

    start project on 17-4-65

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,149 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+function HomePage() {
+  const navigate = useNavigate();
+
+  const [products, setProducts] = useState([]);
+  const [isError, setIsError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const [keywords, setKeywords] = useState("");
+  const [category, setCategory] = useState("");
+
+  const getProducts = async (input) => {
+    try {
+      setIsError(false);
+      setIsLoading(true);
+      const results = await axios.get(
+        `http://localhost:4001/products?keywords=${input.keywords}&category=${input.category}`
+      );
+      setProducts(results.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setIsError(true);
+    }
+  };
+
+  const deleteProduct = async (productId) => {
+    await axios.delete(`http://localhost:4001/products/${productId}`);
+    const newProducts = products.filter((product) => product._id !== productId);
+    setProducts(newProducts);
+  };
+
+  useEffect(() => {
+    getProducts({ keywords, category });
+  }, [keywords, category]);
+
+  return (
+    <div>
+      <div className="app-wrapper">
+        <h1 className="app-title">Products</h1>
+        <button
+          onClick={() => {
+            navigate("/product/create");
+          }}
+        >
+          Create Product
+        </button>
+      </div>
+      <div className="search-box-container">
+        <div className="search-box">
+          <label>
+            Search product
+            <input
+              type="text"
+              placeholder="Search by name"
+              value={keywords}
+              onChange={(e) => setKeywords(e.target.value)}
+            />
+          </label>
+        </div>
+        <div className="category-filter">
+          <label>
+            View Category
+            <select
+              id="category"
+              name="category"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <option disabled value="">
+                -- Select a Category --
+              </option>
+              <option value="it">IT</option>
+              <option value="fashion">Fashion</option>
+              <option value="food">Food</option>
+            </select>
+          </label>
+        </div>
+      </div>
+      <div className="product-list">
+        {!products.length && !isError && (
+          <div className="no-blog-posts-container">
+            <h1>No Products</h1>
+          </div>
+        )}
+        {products.map((product) => {
+          return (
+            <div className="product" key={product._id}>
+              <div className="product-preview">
+                <img
+                  src={product.image}
+                  alt="some product"
+                  width="250"
+                  height="250"
+                />
+              </div>
+              <div className="product-detail">
+                <h1>Product name: {product.name} </h1>
+                <h2>Product price: {product.price}</h2>
+                <h3>Category: {product.category}</h3>
+                <h3>Created Time: {product.created_date}</h3>
+                <p>Product description: {product.description} </p>
+                <div className="product-actions">
+                  <button
+                    className="view-button"
+                    onClick={() => {
+                      navigate(`/product/view/${product._id}`);
+                    }}
+                  >
+                    View
+                  </button>
+                  <button
+                    className="edit-button"
+                    onClick={() => {
+                      navigate(`/product/edit/${product._id}`);
+                    }}
+                  >
+                    Edit
+                  </button>
+                </div>
+              </div>
+
+              <button
+                className="delete-button"
+                onClick={() => {
+                  deleteProduct(product._id);
+                }}
+              >
+                x
+              </button>
+            </div>
+          );
+        })}
+        {isError ? <h1>Request failed</h1> : null}
+        {isLoading ? <h1>Loading ....</h1> : null}
+      </div>
+
+      <div className="pagination">
+        <button className="previous-button">Previous</button>
+        <button className="next-button">Next</button>
+      </div>
+      <div className="pages">1/ total page</div>
+    </div>
+  );
+}
+
+export default HomePage;
